0a3b48bdc87395d38d65121e00cb9ae9a420892d
ahme0543 <ahme0543@algonquinlive.com>
2023-06-12 20:12:58 -0400

    7th

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return src('index.scss')
+  return src('bulma/**/*.scss')
     .pipe(sass())
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['index.scss'], buildStyles)
+  watch(['bulma/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
